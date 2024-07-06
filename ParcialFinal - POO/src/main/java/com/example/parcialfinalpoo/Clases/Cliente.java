1e6f24fc88eef073a1c45a11a429244ec6109519
RafaGdelB23 <00073123@uca.edu.sv>
2024-07-05 20:27:13 -0600

    Clases: Compra, Tarjeta, Cliente

---
+++ ParcialFinal - POO/src/main/java/com/example/parcialfinalpoo/Clases/Cliente.java
@@ -0,0 +1,48 @@
+package com.example.parcialfinalpoo.Clases;
+
+public class Cliente {
+
+    private int id;
+    private String nombreCompleto;
+    private String direccion;
+    private String telefono;
+
+    public Cliente(int id, String nombreCompleto, String direccion, String telefono){
+        this.id = id;
+        this.nombreCompleto = nombreCompleto;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
