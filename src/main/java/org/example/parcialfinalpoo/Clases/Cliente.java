c553d1106dc0f3a80413a7dc833dbcd1f01c2adf
RafaGdelB23 <00073123@uca.edu.sv>
2024-07-07 18:27:46 -0600

    Comentarios de las clases y HelloApplication

--- src/main/java/org/example/parcialfinalpoo/Clases/Cliente.java
+++ src/main/java/org/example/parcialfinalpoo/Clases/Cliente.java
@@ -1,48 +1,33 @@
 package org.example.parcialfinalpoo.Clases;
 
-public class Cliente {
+public class Cliente { //00073123 - Clase cliente de la base de datos
 
-    private int id;
-    private String nombreCompleto;
-    private String direccion;
-    private String telefono;
+    private int id; //00073123 - Atributo ID de tipo entero del cliente
+    private String nombreCompleto; //00073123 - Atributo que guarda el nombre completo del cliente de tipo String
+    private String direccion; // 00073123 - Atributo que guarda la dirección del cliente de tipo String
+    private String telefono; // 00073123 - Atributo que guarda el teléfono del cliente de tipo String
 
-    public Cliente(int id, String nombreCompleto, String direccion, String telefono){
-        this.id = id;
-        this.nombreCompleto = nombreCompleto;
-        this.direccion = direccion;
-        this.telefono = telefono;
+    public Cliente(int id, String nombreCompleto, String direccion, String telefono){ //00073123 - Constructor que permite crear objetos de tipo Cliente
+        this.id = id; //00073123 - Asigna el valor de la variable global id con el parámetro
+        this.nombreCompleto = nombreCompleto; //00073123 - Asigna el valor de la variable global nombreCompleto con el parámetro
+        this.direccion = direccion; //00073123 - Asigna el valor de la variable global dirección con el parámetro
+        this.telefono = telefono; //00073123 - Asigna el valor de la variable global teléfono con el parámetro
     }
 
-    public int getId() {
-        return id;
+    public int getId() { //00073123 - Método getter de tipo entero para hacer referencia al ID
+        return id; //00073123 - Retorna el valor del id del cliente
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public String getNombreCompleto() { //00073123 - Método getter de tipo String para hacer referencia al nombre completo
+        return nombreCompleto; //00073123 - Retorna el valor del nombre completo del cliente
     }
 
-    public String getNombreCompleto() {
-        return nombreCompleto;
+    public String getDireccion() { //00073123 - Método getter de tipo String para hacer referencia a la dirección
+        return direccion; //00073123 - Retorna el valor de la dirección del cliente
     }
 
-    public void setNombreCompleto(String nombreCompleto) {
-        this.nombreCompleto = nombreCompleto;
+    public String getTelefono() { //00073123 - Método getter de tipo String para hacer referencia al teléfono
+        return telefono; //00073123 - Retorna el valor del teléfono del cliente
     }
 
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 }
