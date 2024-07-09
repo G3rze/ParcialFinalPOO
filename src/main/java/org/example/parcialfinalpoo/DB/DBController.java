e71ef5a8a109ff99b4ba70d0391e08d4c4152f17
RafaGdelB23 <00073123@uca.edu.sv>
2024-07-08 21:50:24 -0600

    Comentarios - métodos insert de la clase DBcontroller

--- src/main/java/org/example/parcialfinalpoo/DB/DBController.java
+++ src/main/java/org/example/parcialfinalpoo/DB/DBController.java
@@ -98,86 +98,101 @@ public class DBController {
         return tarjetas; //00055623 Retorna la lista de tarjetas
     }
 
-    public List<Compra> getCompras() {
-        List<Compra> compras = new ArrayList<>();
+    public List<Compra> getCompras() { //00073123 - Método que devuelve una lista de objetos de tipo Compra
+
+        List<Compra> compras = new ArrayList<>(); //00073123 -  Inicizaliza una lista para almacenar objetos de tipo Compra
 
 
-        try {
-            ResultSet resultSet = getDBInstance().con.createStatement().executeQuery("select * from compra");
-            List<Tarjeta> tarjetas = getTarjetas();
-            Tarjeta tarjeta = null;
+        try { //00073123 - Inicio del manejo de errores mediante try catch
 
-            while (resultSet.next()){
-                for(Tarjeta c: tarjetas){
-                    if(c.getId() == resultSet.getInt("id_cliente")){
-                        tarjeta = c;
+            ResultSet resultSet = getDBInstance().con.createStatement().executeQuery("select * from compra"); //00073123 - Ejecuta una consulta SQL para seleccionar los registros de la tabla Compra y los almacena en un ResultSet
+            List<Tarjeta> tarjetas = getTarjetas(); //00073123 - Llama al método getTarjetas para obtener la lista completa de objetos de ese tipo
+            Tarjeta tarjeta = null; //00073123 - Declara una variable de tipo Tarjeta inicializada en null
+
+            while (resultSet.next()){ //00073123 - Bucle while que itera en todas las líneas que encuentre del registro
+
+                for(Tarjeta c: tarjetas){ //00073123 - Bucle for que itera en cada línea
+
+                    if(c.getId() == resultSet.getInt("id_cliente")){ //00073123 - Verifica si el ID de la tarjeta coincide con el ID del cliente en el ResultSet
+
+                        tarjeta = c; //00073123 - Asigna la Tarjeta a la variable tarjeta
                     }
                 }
-                compras.add(new Compra(resultSet.getInt("id"), resultSet.getDate("fechaCompra"), resultSet.getDouble("montoTotal"), resultSet.getString("descripcion"),tarjeta));
+                compras.add(new Compra(resultSet.getInt("id"), resultSet.getDate("fechaCompra"), resultSet.getDouble("montoTotal"), resultSet.getString("descripcion"),tarjeta)); //00073123 - Creación de objeto de tipo Compra con los datos del registro del ResultSet y la tarjeta encontrada, y lo agrega a la lista compras.
             }
 
-            getCon().close();
+            getCon().close(); //00073123 - Cierra la conexión
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        } catch (SQLException e) { //00073123 - Inicio del bloque catch para manejar la excepciones
+
+            throw new RuntimeException(e); //00073123 - En caso de error, lanza un RuntimeException con la excepción original
         }
 
-        return compras;
+        return compras; //00073123 - Devuelve la lista completa de compras
     }
 
-    public void insertClient(Cliente cliente){
-        try {
-            pStatement = getDBInstance().con.prepareStatement("insert into Cliente(nombreCompleto,direccion,telefono) values(?,?,?)");
+    public void insertClient(Cliente cliente){ //00073123 -
 
-            pStatement.setString(1, cliente.getNombreCompleto());
-            pStatement.setString(2, cliente.getDireccion());
-            pStatement.setString(3, cliente.getTelefono());
+        try { //00073123 - Inicio del manejo de errores mediante try catch
 
-            pStatement.execute();
+            pStatement = getDBInstance().con.prepareStatement("insert into Cliente(nombreCompleto,direccion,telefono) values(?,?,?)"); //00073123 - Prepara una consulta de insert para colocar los valores de los campos según los datos dados
 
-            getCon().close();
+            pStatement.setString(1, cliente.getNombreCompleto()); //00073123 - Asigna el valor del nombre completo del cliente en el primer "?"
+            pStatement.setString(2, cliente.getDireccion()); //00073123 - Asigna el valor de la dirección del cliente en el segundo "?"
+            pStatement.setString(3, cliente.getTelefono()); //00073123 - Asigna el valor del teléfono del cliente en el tercer "?"
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+            pStatement.execute(); //00073123 - Ejecuta la consulta select preparada con los valores especificados
 
-    }
+            getCon().close(); //00073123 - Cierra la conexión
 
-    public void insertTarjeta(Tarjeta tarjeta){
-        try {
-            pStatement = getDBInstance().con.prepareStatement("insert into Tarjeta(numeroTarjeta, fechaExpiracion, tipoTarjeta, facilitadorTarjeta, id_cliente) values(?,?,?,?,?)");
+        } catch (SQLException e) { //00073123 - Inicio del bloque catch para manejar la excepciones
 
-            pStatement.setString(1, tarjeta.getNumeroTarjeta());
-            pStatement.setString(2, tarjeta.getFechaExpiracion().toString());
-            pStatement.setString(3, "" + tarjeta.getTipoTarjeta());
-            pStatement.setString(4, tarjeta.getFacilitadorTarjeta());
-            pStatement.setString(5, String.valueOf(tarjeta.getCliente().getId()));
-
-            pStatement.execute();
-
-            getCon().close();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e); //00073123 - En caso de error, lanza un RuntimeException con la excepción original
         }
 
     }
 
-    public void insertCompra(Compra compra){
-        try {
-            pStatement = getDBInstance().con.prepareStatement("insert into Compra(fechaCompra, montoTotal, descripcion, id_tarjeta) values(?,?,?,?)");
+    public void insertTarjeta(Tarjeta tarjeta){ //00073123 -
 
-            pStatement.setString(1, compra.getFechaCompra().toString());
-            pStatement.setString(2, String.valueOf(compra.getMontoTotal()));
-            pStatement.setString(3, compra.getDescripcion());
-            pStatement.setString(4, String.valueOf(compra.getTarjeta().getId()));
+        try { //00073123 - Inicio del manejo de errores mediante try catch
 
-            pStatement.execute();
+            pStatement = getDBInstance().con.prepareStatement("insert into Tarjeta(numeroTarjeta, fechaExpiracion, tipoTarjeta, facilitadorTarjeta, id_cliente) values(?,?,?,?,?)"); //00073123 - Prepara una consulta de insert para colocar los valores de los campos según los datos dados
 
-            getCon().close();
+            pStatement.setString(1, tarjeta.getNumeroTarjeta()); //00073123 - Asigna el valor del número de la tarjeta en el primer "?"
+            pStatement.setString(2, tarjeta.getFechaExpiracion().toString()); //00073123 - Asigna el valor de la fecha de expiración en el segundo "?"
+            pStatement.setString(3, "" + tarjeta.getTipoTarjeta()); //00073123 - Asigna el valor del tipo de la tarjeta en el tercer "?"
+            pStatement.setString(4, tarjeta.getFacilitadorTarjeta()); //00073123 - Asigna el valor del facilitador de la tarjeta en el cuarto "?"
+            pStatement.setString(5, String.valueOf(tarjeta.getCliente().getId())); //00073123 - Asigna el valor del id del cliente en el quinto "?"
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            pStatement.execute(); //00073123 - Ejecuta la consulta select preparada con los valores especificados
+
+            getCon().close(); //00073123 - Cierra la conexión
+
+        } catch (SQLException e) { //00073123 - Inicio del bloque catch para manejar la excepciones
+
+            throw new RuntimeException(e); //00073123 - En caso de error, lanza un RuntimeException con la excepción original
+        }
+
+    }
+
+    public void insertCompra(Compra compra){ //00073123 -
+
+        try { //00073123 - Inicio del manejo de errores mediante try catch
+
+            pStatement = getDBInstance().con.prepareStatement("insert into Compra(fechaCompra, montoTotal, descripcion, id_tarjeta) values(?,?,?,?)"); //00073123 - Prepara una consulta de insert para colocar los valores de los campos según los datos dados
+
+            pStatement.setString(1, compra.getFechaCompra().toString()); //00073123 - Asigna el valor de la fecha de la compra en el primer "?"
+            pStatement.setString(2, String.valueOf(compra.getMontoTotal())); //00073123 - Asigna el valor del monto total de la compra en el segundo "?"
+            pStatement.setString(3, compra.getDescripcion()); //00073123 - Asigna el valor de la descripción de la compra en el tercer "?"
+            pStatement.setString(4, String.valueOf(compra.getTarjeta().getId())); //00073123 - Asigna el valor del id de la tarjeta en el cuarto "?"
+
+            pStatement.execute(); //00073123 - Ejecuta la consulta select preparada con los valores especificados
+
+            getCon().close(); //00073123 - Cierra la conexión
+
+        } catch (SQLException e) { //00073123 - Inicio del bloque catch para manejar la excepciones
+
+            throw new RuntimeException(e); //00073123 - En caso de error, lanza un RuntimeException con la excepción original
         }
     }
 
--- src/main/java/org/example/parcialfinalpoo/HelloApplication.java
+++ src/main/java/org/example/parcialfinalpoo/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException { //0007123 - Método usado en Java para configurar y mostrar la interfaz gráfica del usuario
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));//00073123 - Carga la ventana principal definida por el usuario en un archivo FXML
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240); //Establece las dimensiones y genera la ventana principal
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300); //Establece las dimensiones y genera la ventana principal
         stage.setTitle("Hello!"); //00073123 - Establece el título de la ventana principal de la interfaz
         stage.setScene(scene); //00073123 - Establece la escena de la ventana principal de la interfaz
         stage.show(); //00073123 - Muestra la ventana principal de la interfaz
